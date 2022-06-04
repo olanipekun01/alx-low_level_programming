14cfca9d403823787446df3351aecbf22cf20bd8
Eyitayo <oloniluaeyitayo@gmail.com>
2022-06-04 03:46:31 -0700

    l

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Main point
+ * Description: Description goes here
+ * Return: Return (0) success
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
