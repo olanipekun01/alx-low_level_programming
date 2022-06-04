d446d3a0478fec66549eabc8b0f92884cf1dbd7b
Eyitayo <oloniluaeyitayo@gmail.com>
2022-06-04 04:46:27 -0700

    l

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main point
+ *
+ * Description: description here
+ * Return: success (0)
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'z';
+	while (ch >= 'a')
+		putchar(ch);
+		ch--;
+	putchar('\n');
+	return (0)
+}
