fa74aef2998453b55113b49a07bea12934d3de8a
Eyitayo <oloniluaeyitayo@gmail.com>
2022-06-04 04:30:46 -0700

    l

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main point
+ *
+ * Description - description here
+ * Return: success (0)
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		putchar(n);
+		putchar("\n");
+		n++;
+	}
+	return (0);
+}
