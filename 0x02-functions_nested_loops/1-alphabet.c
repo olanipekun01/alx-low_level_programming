23c0747348544fbdad475ae0fc000ceb2fc60fff
olanipekun01 <oloniluaeyitayo@gmail.com>
2022-06-08 12:59:14 -0700

    l" "

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - main point
+ *
+ * Description: Description goes here
+ * REturn: success (0)
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
