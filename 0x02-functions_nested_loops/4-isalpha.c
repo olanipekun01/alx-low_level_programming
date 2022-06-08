f7069fd37e658ba2f968bd30312cbcceeba9e71c
olanipekun01 <oloniluaeyitayo@gmail.com>
2022-06-08 13:48:00 -0700

    l

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "ctype.h"
+
+/**
+ * _isalpha - check if character is an alphabet
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 if it is an alphabet 0 if otherwise
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c)):
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
