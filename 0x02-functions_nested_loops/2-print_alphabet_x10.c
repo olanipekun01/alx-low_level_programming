7c8d72da8cc565d76de07026c48f86a596127b6b
olanipekun01 <oloniluaeyitayo@gmail.com>
2022-06-08 13:09:35 -0700

    l

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function prints all numbers * 10
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch * 10);
+	}
+	_putchar("\n");
+}
