b6b6c5b7092071b0554c817154fbd25b50933654
olanipekun01 <oloniluaeyitayo@gmail.com>
2022-06-08 12:49:28 -0700

    l

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-	char put[8] = '_putchar';
+	char put[8] = "_putchar";
 	int i;
 
 	for (i = 0; i <= 7; i++)
 	{
 		putchar(put[i]);
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
