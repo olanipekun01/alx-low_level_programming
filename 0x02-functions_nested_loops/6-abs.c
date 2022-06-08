7f460c56c531440c66fca3fedc8dcd8ace2209e7
olanipekun01 <oloniluaeyitayo@gmail.com>
2022-06-08 14:46:07 -0700

    l

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - computes the absolute value
+ *
+ * @c: the number to be checked
+ *
+ * Return: absolute value
+ */
+int _abs(int c)
+{
+	int result = abs(c);
+
+	return (result);
+}
