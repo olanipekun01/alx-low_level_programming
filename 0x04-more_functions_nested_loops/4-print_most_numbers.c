f5fd3b82d0a2989821a3c4e35de47297ae441879
olanipekun01 <oloniluaeyitayo@gmail.com>
2022-06-10 04:10:04 -0700

    l

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,6 +2,7 @@
 
 /**
  * print_most_numbers - a function that prints the numbers, from 0 to 9
+ * Description: prints the numbers excluding 2 and 4
  * Return: 0-9, excluding 2 and 4, followed by new line
  */
 void print_most_numbers(void)
