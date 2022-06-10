b7bbefd2cac90ac3dbc08e4c2d6d65fdbecf257b
olanipekun01 <oloniluaeyitayo@gmail.com>
2022-06-10 05:16:00 -0700

    l

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,22 +3,23 @@
 /**
  * print_line - print the character on the terminal
  * Description: prints the _ character
+ * @n: number of times to print character
  * Return: void
  */
 void print_line(int n)
-{
+i{
 	int i;
 
-	if !(n <= 0)
+	if (n <= 0)
 	{
-		for (i = 0; i < n; i++)
-		{
-			_putchar('_');
-		}
 		_putchar('\n');
 	}
 	else
 	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
 		_putchar('\n');
 	}
 }
