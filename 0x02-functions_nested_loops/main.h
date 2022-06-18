e9b1351cfa7d4ee85302b470fe88f511219509d7
olanipekun01 <56523566+olanipekun01@users.noreply.github.com>
2022-06-17 19:45:29 -0700

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -20,3 +20,7 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int);
 void print_times_table(int);
+void put_unit(int n);
+void put_hundred(int n);
+void put_tens(int n);
+void print_fibonacci(int n);
