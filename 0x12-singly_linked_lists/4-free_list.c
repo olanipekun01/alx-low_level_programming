9a6b27a358967f9b4a104c3c98ab0bca64f5af3e
olanipekun01 <oloniluaeyitayo@gmail.com>
2022-07-15 10:03:39 -0700

    l

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - Frees the heap for a list
+ * @head: linked list
+ * @head: linked list
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp_node;
+
+	while (head != NULL)
+	{
+		tmp_node = head, head = head->next;
+		free(tmp_node->str), free(tmp_node);
+	}  free(head);
+}
