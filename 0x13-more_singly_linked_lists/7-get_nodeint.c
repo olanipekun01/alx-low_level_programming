5d4a67c1ca4d22d936e79da51a20fd182aa0e2e3
olanipekun01 <oloniluaeyitayo@gmail.com>
2022-07-19 13:16:10 -0700

    l

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,8 +2,8 @@
 
 /**
  * get_nodeint_at_index - returns the nth node of a listint_t linked list.
- * head: pointer to the first node in the list
- * head: pointer to the first node in the list
+ * @head: pointer to the first node in the list
+ * @index: index
  * Return: pointer to the indexed node
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
